d517b1910c42a04ace0c16f5d01aed32e3c0baed
Mahmoud Mohamed Ahmed <91031578+Mahmoud-desuky@users.noreply.github.com>
2024-04-24 08:55:45 +0200

    Time: 3 ms (22.77%), Space: 7.1 MB (81.41%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -1,21 +1,14 @@
 class Solution {
 public:
-    int dp[55];
-    
-    int solve(int n)
-    {
-        if(n<2)
-            return n;
-        if(n==2)
-            return 1;
-        int &ret=dp[n];
-        if(~ret)
-            return ret;
-        ret=solve(n-1)+solve(n-2)+solve(n-3);
-        return ret;
-    }
+  
     int tribonacci(int n) {
-        memset(dp,-1,sizeof(dp));
-        return solve(n);
+        int arr[55];
+        arr[0]=0;
+        arr[1]=arr[2]=1;
+        for(int i=3;i<=n;i++)
+        {
+            arr[i]=arr[i-1]+arr[i-2]+arr[i-3];
+        }
+        return arr[n];
     }
 };
