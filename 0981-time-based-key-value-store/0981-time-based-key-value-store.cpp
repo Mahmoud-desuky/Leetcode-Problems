a9de6ce29d8bbc3d0f82e73b0807b825ea0787c5
Mahmoud Mohamed Ahmed <91031578+Mahmoud-desuky@users.noreply.github.com>
2024-08-09 16:02:15 +0300

    Time: 262 ms (72.00%), Space: 133.6 MB (75.70%) - LeetHub

---
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.cpp
@@ -0,0 +1,26 @@
+class TimeMap {
+public:
+    unordered_map<string, vector<pair<int, string>>> mp;
+    TimeMap() {
+    }
+    
+    void set(string key, string value, int timestamp) {
+        mp[key].push_back({timestamp, value});
+    }
+    
+    string get(string key, int timestamp) {
+        if (mp.find(key) == mp.end())
+            return "";
+        auto cmp = [](const pair<int, string>& a, const pair<int, string>& b) {
+            return a.first < b.first;
+        };
+        auto it = upper_bound(mp[key].begin(), mp[key].end(), make_pair(timestamp, ""), cmp);
+        
+        if (it == mp[key].begin())
+            return "";
+        
+        --it;
+        
+        return it->second;
+    }
+};
