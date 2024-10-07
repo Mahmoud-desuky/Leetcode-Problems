6098f5317adde117a50bc24742dc516eee227992
Mahmoud Mohamed Ahmed <91031578+Mahmoud-desuky@users.noreply.github.com>
2024-10-07 16:01:11 +0300

    Time: 4 ms (76.08%), Space: 11.6 MB (31.24%) - LeetHub

---
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int minLength(string s) {
+        stack<char>st;
+        for(auto c:s)
+        {
+            if(c=='B')
+            {
+                if(!st.empty()&&st.top()=='A')
+                    st.pop();
+                else
+                    st.push(c);
+            }
+            else if(c=='D')
+            {
+                if(!st.empty()&&st.top()=='C')
+                    st.pop();
+                else
+                    st.push(c);
+            }
+            else 
+                st.push(c);
+        }
+        return st.size();
+    }
+};
